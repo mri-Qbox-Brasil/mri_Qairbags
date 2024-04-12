248ed4e2f9ca8e01e16aa05c23eff383bf74cb1a
Murai Dev <89431075+mur4i@users.noreply.github.com>
2024-04-12 03:35:43 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game 'gta5'
+
+files {
+    'stream/prop_car_airbag.ytyp',
+}
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_car_airbag.ytyp'
+
+client_script 'cl_airbag.lua'
+
+
